49492a2af70a839385dae8d7bbda940d083c4ac3
Daniel Lindner <daniel.lindner@tum.de>
2019-06-03 12:09:29 +0200

    var2

--- midi_robot_var2/midi_robot_var2.ino
+++ midi_robot_var2/midi_robot_var2.ino
@@ -26,7 +26,7 @@ volatile bool onState;
    t5 ...
 */
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
 void setup()
 {
@@ -51,13 +51,10 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(startButtonInterrupt), toggleStart, FALLING);
 
   //Setup debug
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  Serial.println("Press StartButton to begin.");
+  waitForStart();
 
-  while (!onState) {
-    //Wait for Start
-  }
 
 }
 
@@ -70,33 +67,48 @@ void loop()
     //Serial.println("Active");
   } else {
     stopPlay();
-    Serial.println("Press StartButton to begin.");
+    waitForStart();
   }
 }
 
 void onNoteOn(byte channel, byte pitch, byte velocity)
 {
   //Act on Note recieved
-  if (channel != selectedChannel) {
+  /*if (channel != selectedChannel) {
     return; //Filter for right channel
-  }
+    }*/
 
   switch (pitch) {
+    case 55:
     case 56: playLowG(); break;
+    case 57:
     case 58: playLowA(); break;
+    case 59: playNaturalB(); break;
     case 60: playB(); break;
+    case 61: playNaturalC(); break;
     case 62: playC(); break;
     case 63: playD(); break;
+    case 64: playNaturalE(); break;
     case 65: playE(); break;
+    case 66: playNaturalF(); break;
     case 67: playF(); break;
     case 68: playHighG(); break;
+    case 69: playSharpG(); break;
     case 70: playHighA(); break;
+    case 71: playHighBFlat(); break;
+    case 72: playHighB(); break;
+    case 73: playHighCFlat(); break;
+    case 74: playHighC(); break;
+    case 75: playHighD(); break;
+    case 76: playHighEFlat(); break;
+    case 77: playHighE(); break;
     default: Serial.print("Note: ");
       Serial.print(pitch);
       Serial.println(" not mapped.");
       break;
 
   }
+  //Serial.print("Channel: "); Serial.println(channel);
 }
 
 void onNoteOff(byte channel, byte pitch, byte velocity) {
@@ -113,10 +125,15 @@ void toggleStart() {
   unsigned long interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 300) {
     onState = !onState;
-    Serial.println("Start pressed.");
+    Serial.println("Start/Stop pressed.");
   }
 
   last_interrupt_time = interrupt_time;
+}
+
+void waitForStart() {
+  Serial.println("Press StartButton to begin.");
+  while (!onState) {}
 }
 
 void playLowG()
@@ -147,7 +164,18 @@ void playLowA()
   Serial.println("Playing LowA.");
 }
 
+void playNaturalB()
+{
+  digitalWrite(t2 , HIGH);
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t7 , HIGH);
+  digitalWrite(t9 , HIGH);
 
+  Serial.println("Playing NaturalB.");
+}
 
 void playB()
 {
@@ -159,6 +187,18 @@ void playB()
   digitalWrite(t7 , HIGH);
 
   Serial.println("Playing B.");
+}
+
+void playNaturalC()
+{
+  digitalWrite(t2 , HIGH);
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t8 , HIGH);
+
+  Serial.println("Playing NaturalC.");
 }
 
 void playC()
@@ -184,6 +224,18 @@ void playD()
   Serial.println("Playing D.");
 }
 
+void playNaturalE()
+{
+  digitalWrite(t2 , HIGH);
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t7 , HIGH);
+  digitalWrite(t8 , HIGH);
+
+  Serial.println("Playing NaturalE.");
+}
+
 void playE()
 {
   digitalWrite(t2 , HIGH);
@@ -194,6 +246,18 @@ void playE()
   digitalWrite(t8 , HIGH);
 
   Serial.println("Playing E.");
+}
+
+void playNaturalF()
+{
+  digitalWrite(t2 , HIGH);
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t7 , HIGH);
+  digitalWrite(t8 , HIGH);
+
+  Serial.println("Playing NaturalF.");
 }
 
 void playF()
@@ -210,11 +274,21 @@ void playF()
 void playHighG()
 {
   digitalWrite(t2 , HIGH);
-  digitalWrite(t6 , HIGH);
-  digitalWrite(t7 , HIGH);
-  digitalWrite(t8 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
 
   Serial.println("Playing HighG.");
+}
+
+void playSharpG()
+{
+  digitalWrite(t2 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t8 , HIGH);
+
+  Serial.println("Playing SharpG.");
 }
 
 void playHighA()
@@ -225,6 +299,79 @@ void playHighA()
   digitalWrite(t8 , HIGH);
 
   Serial.println("Playing HighA.");
+}
+
+void playHighBFlat()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t7 , HIGH);
+  digitalWrite(t9 , HIGH);
+
+  Serial.println("Playing HighBFlat.");
+}
+
+void playHighB()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t7 , HIGH);
+
+  Serial.println("Playing HighB.");
+}
+
+void playHighCFlat()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+  digitalWrite(t8 , HIGH);
+
+  Serial.println("Playing HighCFlat.");
+}
+
+void playHighC()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t6 , HIGH);
+
+  Serial.println("Playing HighC.");
+}
+
+void playHighD()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+
+  Serial.println("Playing HighD.");
+}
+
+void playHighEFlat()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t5 , HIGH);
+  digitalWrite(t7 , HIGH);
+  digitalWrite(t9 , HIGH);
+
+  Serial.println("Playing HighEFlat.");
+}
+
+void playHighE()
+{
+  digitalWrite(t3 , HIGH);
+  digitalWrite(t4 , HIGH);
+  digitalWrite(t6 , HIGH);
+
+  Serial.println("Playing HighE.");
 }
 
 void stopPlay()
@@ -238,4 +385,6 @@ void stopPlay()
   digitalWrite(t7, LOW);
   digitalWrite(t8, LOW);
   digitalWrite(t9, LOW);
+
+  Serial.println("Stopped.");
 }
